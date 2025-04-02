4c33f6ced3600593666d5fa475cdf256fd83fdd5
Thomas Taulo <taulo@live.dk>
2025-04-02 15:33:21 +0200

    daily

---
+++ Daily/Easy/Maximum-Value-of-an-Ordered-Triplet-I.js
@@ -0,0 +1,13 @@
+var maximumTripletValue = function(nums) {
+    let max = 0
+    for (let i = 0; i < nums.length - 2; i++) {
+        for (let j = i + 1; j < nums.length - 1; j++) {
+            for (let k = j + 1; k < nums.length; k++) {
+                const sum = (nums[i] - nums[j]) * nums[k]
+                max = Math.max(sum, max)
+            }
+        }
+    }
+
+    return max
+};
