663b2b97a1f88ea9ae5550138c4b94cf590b1713
Thomas Taulo <taulo@live.dk>
2025-04-02 14:59:06 +0200

    2 problems

---
+++ Algorithms/Medium/Minimum Time to Complete Trips.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} time
+ * @param {number} totalTrips
+ * @return {number}
+ */
+var minimumTime = function(time, totalTrips) {
+    let count = 0;
+
+    for (let i = 0; i < totalTrips; i++) {
+
+    }
+
+    return count;
+}
+
+const c = minimumTime([1,2,3], 5)
+
+console.log(c)
